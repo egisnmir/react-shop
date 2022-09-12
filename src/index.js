03442a2dc413021bc6a2fbbdb73ff219cfce49d8
Egis <egis@egis.egis>
2022-09-12 15:14:17 +0300

    root render update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-render(
-  <React.Fragment>
-      <App />
-  </React.Fragment>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
