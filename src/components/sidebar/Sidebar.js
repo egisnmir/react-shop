35f0bd99e33abe7d4ebd2f06fa6c08e735db8215
Egis <egis@egis.egis>
2022-09-14 15:43:44 +0300

    refactoring

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,10 +3,7 @@ import './Sidebar.scss';
 const Sidebar = () => {
 
   return (
-    <div className="sidebar">
-      <h4>Sidebar</h4>
-      <p>Paragraph of text</p>
-    </div>
+    <div className="sidebar"></div>
   )
 }
 
