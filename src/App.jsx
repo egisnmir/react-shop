7e4e8eff07bfb68ee1da0b4a533b4f3a5175a5bb
Egis <egis@egis.egis>
2022-09-18 21:23:52 +0300

    added total number of items in cart to nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.scss';
 import { Outlet, Link } from 'react-router-dom';
 import Sidebar from './components/sidebar/Sidebar';
+import CartContext from './components/contexts/CartContext';
+import { useContext } from 'react';
 
 function App() {
+  const { totalAmount } = useContext(CartContext);
+
   return (
     <div className="app">
       <Sidebar></Sidebar>
@@ -11,7 +15,9 @@ function App() {
         <nav>
           <Link to='/'>Home</Link>
           <Link to='/products'>Products</Link>
-          <Link to='/cart'>Cart</Link>
+          <Link to='/cart'>
+            Cart{totalAmount > 0 ? ` [${totalAmount}]` : ''}
+          </Link>
           <Link to='/favorites'>Favorites</Link>
         </nav>
         
