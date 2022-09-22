904c465f41d85a1c664959973d3ee15a2d222d95
Egis <egis@egis.egis>
2022-09-22 09:24:03 +0300

    redux refactor

--- src/core/actions/action.js
+++ src/core/actions/action.js
@@ -1,7 +1,9 @@
+import { INCREMENT, DECREMENT } from "../types/counter";
+
 export const increment = () => {
-    return { type: 'INCREMENT' };
+    return { type: INCREMENT };
 }
 
 export const decrement = () => {
-    return { type: 'DECREMENT' };
+    return { type: DECREMENT };
 }
--- src/core/reducers/countReducer.js
+++ src/core/reducers/countReducer.js
@@ -6,19 +6,15 @@ const initialState = {
 export const countReducer = (state = initialState, action) => {
     switch(action.type) {
         case 'INCREMENT':
-        {
-            return { 
-                ...state,
-                count: state.count + 1
-            };
-        }
+        return { 
+            ...state,
+            count: state.count + 1
+        };
         case 'DECREMENT':
-        {
-            return {
-                ...state,
-                count: state.count - 1
-            };
-        }
+        return {
+            ...state,
+            count: state.count - 1
+        };
         default:
             return state;
     }
