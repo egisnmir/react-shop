d76de8746354825d190ff864022790a505766eb3
Egis <egis@egis.egis>
2022-10-13 14:26:33 +0300

    homepage text update

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -9,6 +9,8 @@ export default function HomePage() {
             <p>Some parts use <b>context</b> and others use <b>Redux (toolkit)</b>. While not necesary, or even correct, this is done on purpose just for the sake of it.</p>
             <p>There is no UI design, but I was considering using <b>Tailwind</b> for styling.</p>
             <p>A list of "things to do" is in the README.md file.</p>
+            <br/>
+            <p>Use the navbar to go between pages.</p>
         </main>
     )
 }
