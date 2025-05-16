d372149bad51ebabf2107471b818e3aac35e36db
Egis <egis@egis.egis>
2025-05-16 14:30:12 +0300

    readme updated

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -3,12 +3,10 @@ export default function HomePage() {
         <main>
             <h4>Home page</h4>
 
-            <p>This is a showcase of React functionality and code.</p>
-            <p>Some parts use <b>context</b> and others use <b>Redux (toolkit)</b>. While not necesarily required, this is done for test purposes.</p>
-            <p>There is no UI design, but I was considering using <b>Tailwind</b> for styling.</p>
+            <p>A showcase of React functionality and code.</p>
+            <p>Some parts use <b>context</b> and others use <b>Redux (toolkit)</b>. It's done this way for test purposes.</p>
+            <p>No UI design in this app.</p>
             <p>A list of "things to do" is in the README.md file.</p>
-            <br/>
-            <p>Use the navbar to go between pages.</p>
         </main>
     )
 }
